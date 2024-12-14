82d8cdabfd72971abe20b788e41c31de09f4358e
Stanislav <stanich81@gmail.com>
2024-12-14 21:40:08 +0300

    new

--- src/Eagle.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate;
-import java.util.List;
-
-public class Eagle extends Animal{
-
-    public Eagle(String name, LocalDate birthday, List<String> vaccination, String illness, String owner) {
-        super(name, birthday, vaccination, illness, owner);
-    }
-}
